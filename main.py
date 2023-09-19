c2ca795c3f702c47434c6cd17ce661154dc5c131
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-09-19 15:15:51 +0200

    update SQLlite
    update SQL

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
+from flask import Flask, Response, redirect, render_template, request, url_for
+from database import db
+import os
+
 import helper
-import datetime
-from flask import Flask, request, Response, render_template, redirect, url_for
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db.init_app(app)
+app.app_context().push()
+db.create_all()
+
+
+@app.route("/getCSV")
+def get_csv():
+    return Response(
+        helper.get_csv(),
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=zu-bbbearbeiten.csv"},
+    )
+
 
 @app.route("/")
 def index():
-    items = helper.get_all()
-    return render_template('index.html', items=items)
+    items = helper.get_all(sorted=True)
+    return render_template("index.html", items=items)
 
 
-@app.route('/add', methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     text = request.form.get("text")
-    date_str = request.form.get("date")
-    date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()  
-    helper.add(text, date)
+    date = request.form.get("deadline")
+    category = request.form.get("category")
+    description = request.form.get("description")
+    helper.add(text, date=date, category=category, description=description)
     return redirect(url_for("index"))
 
 
-
-@app.route('/update/<int:index>')
+@app.route("/update/<int:index>")
 def update(index):
     helper.update(index)
     return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
