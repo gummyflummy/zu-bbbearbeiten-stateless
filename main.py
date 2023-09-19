27027709a6884eae0ce101d4c905c18a763cc852
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-09-19 16:47:22 +0200

    postgresql fail update
    doesn't work

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import helper
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:hallo@localhost:5432/todo"
 db.init_app(app)
 app.app_context().push()
 db.create_all()
